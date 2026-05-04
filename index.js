35b33962279ad58766be8157e399347217eaa5a6
ducckyyt-sudo <ducckyyt@gmail.com>
2026-05-03 19:16:45 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const { joinVoiceChannel, createAudioPlayer, createAudioResource } = require('@discordjs/voice');
+const fs = require('fs');
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates]
+});
+
+client.once('clientReady', async () => {
+  console.log(`Logged in as ${client.user.tag}`);
+
+    const guild = client.guilds.cache.get('1424473149138796566');
+    const channel = guild.channels.cache.get('1455378516853129309');
+
+  const connection = joinVoiceChannel({
+    channelId: channel.id,
+    guildId: guild.id,
+    adapterCreator: guild.voiceAdapterCreator,
+  });
+
+  const player = createAudioPlayer();
+
+  // Play a silent audio file (you need to add one)
+  const resource = createAudioResource('silent.mp3');
+
+  player.play(resource);
+  connection.subscribe(player);
+});
+
+client.login('MTUwMDY1ODgwMDcyMDM1MTM0Mw.GZhY4V.MEH6hsHwKX_o8zepfAAMo9sxG7wisMZiyzRtSQ');
